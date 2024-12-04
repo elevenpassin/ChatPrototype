2a7af0fc8830d451fe68f804eee869a87188d0b0
Alice Kile <buoyantair@gmail.com>
2024-12-04 22:12:54 +0100

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Alice Kile on 04.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
