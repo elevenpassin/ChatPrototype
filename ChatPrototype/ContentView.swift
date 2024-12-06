195cd259e7e684f939cea0c61b78213c0ca8615e
Alice Kile <buoyantair@gmail.com>
2024-12-06 11:05:57 +0100

    La chat screen

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 
+struct MessageView: View {
+    var body: some View {
+        HStack(alignment: VerticalAlignment.center) {
+            Circle()
+                .frame(width: 50, height: 50)
+                .border(.green)
+            Text("Hello world")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: HorizontalAlignment.leading, spacing: 15) {
+            MessageView()
+            MessageView()
+            MessageView()
         }
-        .padding()
     }
 }
 
